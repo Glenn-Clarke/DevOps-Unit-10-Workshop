e0d21f58d5a44d199d407d7393b319d824dd700a
Glenn Clarke <glenn.clarke@softwire.com>
2025-09-03 15:24:50 +0100

    Error alerts

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -28,17 +28,19 @@ def process_orders(app):
             "customer": order.customer,
             "date": order.date_placed_local.isoformat(),
         }
+        try:
+            response = requests.post(
+                app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
+                json=payload
+            )
+            app.logger.info("Response from endpoint: " + response.text)
+            app.logger.info("Date placed: " + order.date_placed.isoformat())
+            response.raise_for_status()
 
-        response = requests.post(
-            app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
-            json=payload
-        )
-        app.logger.info("Response from endpoint: " + response.text)
-        app.logger.info("Date placed: " + order.date_placed.isoformat())
-        response.raise_for_status()
-
-        order.set_as_processed()
-        save_order(order)
+            order.set_as_processed()
+            save_order(order)
+        except:
+            app.logger.exception("Error processing order {id}".format(id=order.id))
 
 def get_queue_of_orders_to_process():
     allOrders = get_all_orders()
